3321df98fdcd5c6c9bf9575a14f6f5357dad14b3
Joshua Morton <67209256+jMorton95@users.noreply.github.com>
2024-12-16 21:52:44 +0000

    [ch] re-add config

---
+++ raw-api/Config/ConfigureAppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace RAWAPI.Config;
+
+public static class ConfigureAppSettings
+{
+    public static void ConfigureOptions(this WebApplicationBuilder builder)
+    {
+        //builder.Services.Configure<ConnectionStringSettings>(builder.Configuration.GetSection("ConnectionString"));
+    }
+}
