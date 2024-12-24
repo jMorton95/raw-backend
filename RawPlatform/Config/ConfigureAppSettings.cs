c7c2d4005b164e12ff1a14bc21d32d15a1562a4f
Josh Morton <josh.morton@propeltech.co.uk>
2024-12-24 10:42:40 +0000

    [dev] test env variables piping

--- RawPlatform/Config/ConfigureAppSettings.cs
+++ RawPlatform/Config/ConfigureAppSettings.cs
@@ -1,9 +1,15 @@
 ﻿namespace RawPlatform.Config;
 
+public class TestSettings()
+{
+    public string TestValue { get; set; }
+}
+
 public static class ConfigureAppSettings
 {
     public static void ConfigureOptions(this WebApplicationBuilder builder)
     {
-        //builder.Services.Configure<ConnectionStringSettings>(builder.Configuration.GetSection("ConnectionString"));
+      //builder.Services.Configure<ConnectionStringSettings>(builder.Configuration.GetSection("ConnectionString"));
+      builder.Services.Configure<TestSettings>(builder.Configuration.GetSection("TestSettings"));   
     }
 }
