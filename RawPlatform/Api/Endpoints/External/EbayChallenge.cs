0c6cae1750f6f30494796a3e318e5ef2db035fff
Joshua Morton <67209256+jMorton95@users.noreply.github.com>
2024-12-30 13:54:37 +0000

    [dev] add ChallengeEndpoint

--- RawPlatform/Api/Endpoints/External/EbayChallenge.cs
+++ RawPlatform/Api/Endpoints/External/EbayChallenge.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using RawPlatform.Config.Models;
 
@@ -11,9 +13,17 @@ public class EbayChallenge : IEndpoint
         app.MapGet("/third-party/challenge", Handler);
     }
 
-    private static Task<Results<Ok<Response>, UnauthorizedHttpResult>> Handler([FromQuery] string challenge_code)
+    private static Results<Ok<Response>, InternalServerError> Handler([FromQuery] string challenge_code, [FromServices] EbayChallengeService service)
     {
-        
+        try
+        {
+            var challengeResponse = service.VerifyChallenge(challenge_code);
+            return TypedResults.Ok(new Response(challengeResponse));
+        }
+        catch (Exception ex)
+        {
+            return TypedResults.InternalServerError();
+        }
     }
 
     private record Response(string ChallengeResponse);
@@ -21,5 +31,19 @@ public class EbayChallenge : IEndpoint
 
 public class EbayChallengeService(IOptions<ThirdParty> apiSettings)
 {
-    
+    private readonly ThirdParty _apiSettings = apiSettings.Value;
+    public string VerifyChallenge(string challengeCode)
+    {
+        if (_apiSettings.VerificationToken is null || _apiSettings.HostedEndpoint is null)
+        {
+            throw new ArgumentException("Invalid Configuration");   
+        }
+        
+        var challengeHash = IncrementalHash.CreateHash(HashAlgorithmName.SHA256); 
+        challengeHash.AppendData(Encoding.UTF8.GetBytes(challengeCode)); 
+        challengeHash.AppendData(Encoding.UTF8.GetBytes(_apiSettings.VerificationToken)); 
+        challengeHash.AppendData(Encoding.UTF8.GetBytes(_apiSettings.HostedEndpoint)); 
+        return Convert.ToHexStringLower(challengeHash.GetHashAndReset());
+    }
+        
 }
--- RawPlatform/Config/Models/ThirdParty.cs
+++ RawPlatform/Config/Models/ThirdParty.cs
@@ -3,5 +3,6 @@
 public class ThirdParty()
 {
     public string? ApiKey { get; init; }
-    public string? ChallengeCode { get; init; }
+    public string? VerificationToken { get; init; }
+    public string? HostedEndpoint {get; init; }
 }
